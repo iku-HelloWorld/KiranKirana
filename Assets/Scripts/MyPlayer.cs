122eeb131cb81e6f24a5b80ef0f1fdc9eecf1589
omergenckzn <omergenc.me@gmail.com>
2022-08-01 13:46:00 +0300

    Revert "PlayerCameraWork"
    This reverts commit 3e3784026e088283c1f42872a08e7a13242ecc82.

--- Assets/Scripts/MyPlayer.cs
+++ Assets/Scripts/MyPlayer.cs
@@ -4,29 +4,19 @@ using UnityEngine;
 
 public class MyPlayer : MonoBehaviour
 {
-    public float moveSpeed = 8f;
-    public float smoothRotationTime = 0.25f;
-    float currentVelocity;
+    
 
-    float currentSpeed;
-    float speedVelocity;
-
+    
     void Update()
     {
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         Vector2 inputDir = input.normalized;
 
         if(inputDir != Vector2.zero)
-        {
-            float rotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
-            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, rotation, ref currentVelocity, smoothRotationTime);
-            
-        }
+        transform.eulerAngles = Vector3.up * Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
 
 
-        float targetSpeed = moveSpeed * inputDir.magnitude;
-        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedVelocity, 0.1f);
-        transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
+        transform.Translate(transform.forward * (5f*inputDir.magnitude) * Time.deltaTime, Space.World);
         
     }
 }
