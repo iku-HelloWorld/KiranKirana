3e3784026e088283c1f42872a08e7a13242ecc82
omergenckzn <omergenc.me@gmail.com>
2022-08-01 13:41:28 +0300

    PlayerCameraWork

--- Assets/Scripts/MyPlayer.cs
+++ Assets/Scripts/MyPlayer.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class MyPlayer : MonoBehaviour
 {
-    
+    public float moveSpeed = 8f;
+    public float smoothRotationTime = 0.25f;
+    float currentVelocity;
 
-    
+    float currentSpeed;
+    float speedVelocity;
+
     void Update()
     {
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         Vector2 inputDir = input.normalized;
 
         if(inputDir != Vector2.zero)
-        transform.eulerAngles = Vector3.up * Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
+        {
+            float rotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, rotation, ref currentVelocity, smoothRotationTime);
+            
+        }
 
 
-        transform.Translate(transform.forward * (5f*inputDir.magnitude) * Time.deltaTime, Space.World);
+        float targetSpeed = moveSpeed * inputDir.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedVelocity, 0.1f);
+        transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
         
     }
 }
