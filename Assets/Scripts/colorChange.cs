a779d0deda5cbc400c5117e18bf26b7faec7b171
alwaysad <73255665+alwaysad@users.noreply.github.com>
2022-08-01 15:08:29 +0300

    a

---
+++ Assets/Scripts/colorChange.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChange : MonoBehaviour
+{
+    bool trueA;
+    bool trueB;
+
+    bool answered = true;
+
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trueA = FindObjectOfType<GameManager>().rightA;
+
+        trueB = FindObjectOfType<GameManager>().rightB;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        answered = true;
+
+        if ((collision.gameObject.tag == "Player" && trueA && transform.gameObject.tag == "A") || (collision.gameObject.tag == "Player" && trueB && transform.gameObject.tag == "B"))
+        {
+
+            transform.GetChild(0).GetComponent<Light>().color = Color.green;
+
+        }
+        else
+        {
+            transform.GetChild(0).GetComponent<Light>().color = Color.red;
+        }
+
+
+
+
+    }
+}
