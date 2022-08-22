1a2a52b6aec40f6078d27fd0bbffa0aa391fa382
alwaysad <73255665+alwaysad@users.noreply.github.com>
2022-08-22 11:50:56 +0300

    ses olayı düzeltildi

---
+++ Assets/customProperties.cs
@@ -0,0 +1,38 @@
+
+using UnityEngine;
+using Photon.Pun.Demo.PunBasics;
+using Photon.Pun.UtilityScripts;
+using Photon.Pun;
+using ExitGames.Client.Photon;
+public class customProperties : MonoBehaviour
+{
+
+    //Hashtable props;
+    //bool stillWaiting;
+    //bool answerReveal;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+       
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    Debug.Log("hala bekliyor mu " + stillWaiting);
+    //    Debug.Log("answer reveal" + answerReveal);
+    //    stillWaiting = FindObjectOfType<deathHandler>().stillwaiting;
+    //    answerReveal = FindObjectOfType<GameManager>().answerReveal;
+    //    //if (answerReveal)
+    //    //{
+           
+    //    //        PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue("alive", out object isAlive);
+    //    //        props["alive"] = !(bool)isAlive;
+    //    //        PhotonNetwork.LocalPlayer.SetCustomProperties(props);
+    //    //        Debug.Log("player gg");
+            
+    //    //}
+    //}
+}
